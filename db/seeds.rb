6870560753824f87733571ff956fceea306eb33e
Melo <78803336+mellotario@users.noreply.github.com>
2024-07-10 09:24:45 -0500

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,50 @@
+# db/seeds.rb
 require 'open-uri'
 require 'json'
 
-Category.create(name: 'Fiction')
-Category.create(name: 'Non-Fiction')
+# Create Categories
+fiction = Category.find_or_create_by(name: 'Fiction')
+non_fiction = Category.find_or_create_by(name: 'Non-Fiction')
 
-categories = Category.all
+# Create Authors
+authors = [
+  { name: 'George Orwell', biography: 'George Orwell was an English novelist and journalist.' },
+  { name: 'J.K. Rowling', biography: 'J.K. Rowling is a British author, best known for the Harry Potter series.' }
+].map { |author_attrs| Author.find_or_create_by(author_attrs) }
 
+# Fetch Data from Open Library API
 isbns = ['0451526538', '0385472579', '0201558025']
+categories = [fiction, non_fiction]
 
 isbns.each do |isbn|
   url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{isbn}&format=json&jscmd=data"
   response = URI.open(url).read
   book_data = JSON.parse(response)["ISBN:#{isbn}"]
 
+  # Ensure we have a valid publish_date
+  publish_date = book_data['publish_date'] || Faker::Date.between(from: '1900-01-01', to: Date.today)
+
   Book.create(
     title: book_data['title'],
-    author: book_data['authors'][0]['name'],
-    published_date: book_data['publish_date'],
     isbn: isbn,
     cover_url: book_data.dig('cover', 'large'),
     category: categories.sample,
+    author: authors.sample,
+    published_date: publish_date,
     latitude: Faker::Address.latitude,
     longitude: Faker::Address.longitude
   )
 end
 
+# Create Additional Books with Faker
 100.times do
   Book.create(
     title: Faker::Book.title,
-    author: Faker::Book.author,
-    published_date: Faker::Date.between(from: '1900-01-01', to: Date.today),
     isbn: Faker::Number.unique.number(digits: 10).to_s,
     cover_url: 'https://via.placeholder.com/150',
     category: categories.sample,
+    author: authors.sample,
+    published_date: Faker::Date.between(from: '1900-01-01', to: Date.today),
     latitude: Faker::Address.latitude,
     longitude: Faker::Address.longitude
   )
